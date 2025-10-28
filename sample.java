283b6abe63e71ef4fab3405a6d3bfe27770adea7
Dakshith Saraf <dakshithsarafbsc24@rvu.edu.in>
2025-10-28 10:06:56 +0530

    Correct output statement for sum of x and y
    Fix typo in 'System' and correct sum output format.

--- sample.java
+++ sample.java
@@ -3,5 +3,7 @@ public class sample {
     {
         System.out.println("Hello, World!");
         System.out.println("Hi bro"); //my first commit
+        int x=4, y=5;
+        system.out.println("the sum of x and y are:",x+y);
     }
 }
