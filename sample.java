bbc828568031369ae83f88af562b9e065b07e462
hemanthbybsc24-oss <hemanthbybsc24@rvu.edu.in>
2025-10-29 21:11:09 +0530

    Refactor main method to handle student input
    Removed initial print statements and added student input loop.

--- sample.java
+++ sample.java
@@ -1,9 +1,8 @@
 public class sample {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
-        System.out.println("Hi bro"); //my first commit
-        int x=4, y=5;
-        system.out.println("the sum of x and y are:",x+y);
-    }
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+        System.out.print("\nEnter name of Student " + (i + 1) + ": ");
+}
 }
